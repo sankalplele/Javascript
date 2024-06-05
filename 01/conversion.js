440f16f2ba04a1f7d6c8b053d5d75f83cdad537f
sankalplele <sankalplele2003@gmail.com>
2024-06-05 08:09:20 +0000

    x

---
+++ 01/conversion.js
@@ -0,0 +1,20 @@
+let score = "g"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+let isLoggedIn = "xd";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//if 1 => true; 0 => false
+// if "" => false; "dsfas" => true
+
+let someNumber = "33"
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
