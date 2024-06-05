e4915d98ede838a781fc69bf45d8f98057fe9461
sankalplele <sankalplele2003@gmail.com>
2024-06-05 09:23:07 +0000

    x

--- 01/conversion.js
+++ 01/conversion.js
@@ -18,3 +18,31 @@ let someNumber = "33"
 let stringNumber = String(someNumber)
 
 console.log(typeof stringNumber)
+
+//*******Operations on Datatypes *********/
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2**3); //exponent
+
+
+let str1 = "hello"
+let str2 = "randy"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2" );
+console.log("1" + 2 + 2 );
+console.log(1 + 2 + "2" );
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+// === for strict comaparison for example if we compare a string with a number this operator will not convert any of them and hence conclude that they are uncomparable
+
--- 01/datatypes.js
+++ 01/datatypes.js
@@ -1,1 +1,8 @@
 "use strict";
+
+//primitive datatypes
+//7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference (Non-primitive)
+// Arrays, Objects, Functions
+
