6ef6fa55faa2152a890239b275844cfbb9be0703
sankalplele <sankalplele2003@gmail.com>
2024-06-05 07:54:28 +0000

    c

---
+++ 01/variables.js
@@ -0,0 +1,10 @@
+const accountId = 14455
+let accountEmail = "xyz@fff.com"
+var accPass = "1234"
+accCity = "Jaipur"
+
+//accountId = 2
+accountEmail = "ggggg"
+accPass = "333"
+
+console.table([accountId,accPass,accountEmail,accCity])
